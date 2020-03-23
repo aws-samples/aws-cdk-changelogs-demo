51a4ba6f66d85f66b1c43790a57302a6e4661c93
Nathan Peck <peckn@amazon.com>
2020-03-23 13:48:05 -0400

    Fixing package versions

--- app/test/test-pypi.js
+++ app/test/test-pypi.js
@@ -5,26 +5,26 @@ describe.only('PyPI', function() {
   var rawXML;
   var packageNames;
 
-  it.skip('should be able to fetch raw XML of recent packages feed', async function() {
+  it('should be able to fetch raw XML of recent packages feed', async function() {
     this.timeout(10000);
     rawXML = await PyPI.fetchXMLFromPyPI();
     expect(rawXML).to.be.a('string');
   });
 
-  it.skip('should be able to parse package names out of the XML', async function() {
+  it('should be able to parse package names out of the XML', async function() {
     packageNames = PyPI.parsePackageNamesFromXML(rawXML);
 
     expect(packageNames).to.be.an('array');
     expect(packageNames.length).to.be.greaterThan(0);
   });
 
-  it.skip('should be able to fetch the Github for a package', async function() {
+  it('should be able to fetch the Github for a package', async function() {
     this.timeout(10000);
     var packageRepo = await PyPI.packageNameToRepo('cognite-sdk');
     expect(packageRepo).to.equal('cognitedata/cognite-sdk-python');
   });
 
-  it('should upsert recent packages from the entry point', async function() {
+  it.skip('should upsert recent packages from the entry point', async function() {
     this.timeout(10000);
 
     await PyPI.upsertRecentPackageRepos();
